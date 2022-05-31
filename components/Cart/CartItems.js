9b5ddd72ac609ab953c06ec32e80228d396dfc91
Prince Ahmed <codingwithprince@gmail.com>
2022-05-31 20:42:35 +0600

    added user image instead of name:

--- components/Cart/CartItems.js
+++ components/Cart/CartItems.js
@@ -14,13 +14,27 @@ const CartItems = () => {
     
   },[myState.length, router])
 
+  const userState = useSelector(state => state.userReducer)
+    const checkoutHandler =()=>{
+        router.push('/login')
+    }
+
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center'>
-      {
-        myState.map(item =>  <SingleFoodItem key={item.id} remove={true} item={item} /> )
-      }
-       
+    <div className='px-[15%]'>
+    <div className='flex flex-wrap justify-between items-center'>
+      <h2 className='block text-4xl font-semibold text-[#444] py-10'>Your cart</h2>
+      <button onClick={()=> checkoutHandler()} className='inline-block shadow-lg bg-[#f91944] hover:bg-red-500 text-white font-bold rounded-full py-2 px-5'>Checkout</button>
     </div>
+    
+    <div className="cart-items">
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center'>
+  {
+    myState.map(item =>  <SingleFoodItem key={item.id} remove={true} item={item} /> )
+  }
+   
+</div>
+    </div>
+</div>
   )
 }
 
--- pages/cart.js
+++ pages/cart.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { useRouter } from 'next/router'
 import CartItems from '../components/Cart/CartItems'
 
 const cart = () => {
-    const userState = useSelector(state => state.userReducer)
-    const router = useRouter()
-
-    const checkoutHandler =()=>{
-        router.push('/login')
-    }
+    
   return (
-    <div className='px-[15%]'>
-        <div className='flex flex-wrap justify-between items-center'>
-          <h2 className='block text-4xl font-semibold text-[#444] py-10'>Your cart</h2>
-          <button onClick={()=> checkoutHandler()} className='inline-block shadow-lg bg-[#f91944] hover:bg-red-500 text-white font-bold rounded-full py-2 px-5'>Checkout</button>
-        </div>
-        
-        <div className="cart-items">
-            <CartItems />
-        </div>
-    </div>
+    <CartItems />
   )
 }
 
